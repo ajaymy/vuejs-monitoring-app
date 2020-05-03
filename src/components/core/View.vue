d3e73b11038367b1cf582d73f0510180903b4b75
A J <ajaymy@gmail.com>
2020-05-03 16:22:37 +0800

    Add files via upload

---
+++ src/components/core/View.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-content>
+    <div id="core-view">
+      <v-fade-transition mode="out-in">
+        <keep-alive>
+          <router-view />
+        </keep-alive>
+      </v-fade-transition>
+    </div>
+    <!-- <core-footer v-if="$route.name !== 'Maps'" /> -->
+  </v-content>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: 'Production Monitoring System'
+    };
+  }
+};
+</script>
+
+<style>
+#core-view {
+  padding-bottom: 100px;
+}
+</style>
